194c8f17f59ed04f6f836a54290de9534c239bbb
Sabbir Hosen <18201076@uap-bd.edu>
2022-02-10 10:39:40 +0600

    C-LOOKUP done and C-SCAN problem fixed

--- disk/C_LOOKUP.py
+++ disk/C_LOOKUP.py
@@ -1,16 +1,12 @@
 queue = list(map(int, input('Enter request : ').split()))
 header = int(input('Head Starting Positions : '))
-lower_bound = int(input('Enter lower bound: '))
-upper_bound = int(input('Enter upper bound: '))
 u_queue = queue
 u_queue.append(header)
-u_queue.append(lower_bound)
-u_queue.append(upper_bound)
 u_queue.sort()
 
 lower_queue = u_queue[: u_queue.index(header)]
-lower_queue.insert(0, upper_bound)
 upper_queue = u_queue[u_queue.index(header):]
+lower_queue.insert(0, upper_queue[-1])
 total = 0
 calculating_string = ''
 for i in range(0, len(upper_queue)-1):
@@ -24,7 +20,7 @@ for i in range(0, len(lower_queue)-1):
         calculating_string = calculating_string + '|' + str(lower_queue[i]) + '-' + str(lower_queue[i + 1]) + '|'
     else:
         calculating_string = calculating_string + '|' + str(lower_queue[i]) + '-' + str(lower_queue[i+1]) + '|' + '+'
-path = upper_queue + lower_queue
+path = upper_queue + lower_queue[1:]
 print('Path: ', ' '.join(list(map(str, path))))
 print('Calculation: ', calculating_string)
 print('Total head movement {}'.format(total))
